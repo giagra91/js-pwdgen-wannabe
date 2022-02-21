2f635ac158d00aaa6972045e806f10f121c3d213
giagra91 <giagra91@gmail.com>
2022-02-21 16:31:42 +0100

    modfiche nomi delle variabili

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Variabile Nome
-let firstName = prompt("Qual è il tuo nome?");
+const firstName = prompt("Qual è il tuo nome?");
 console.log(firstName);
 
 // Variabile Cognome
-let surname = prompt("Qual è il tuo cognome?");
+const surname = prompt("Qual è il tuo cognome?");
 console.log(surname);
 
 // Variabile Colore
-let myColor = prompt("Qual è il tuo cognome?");
+const myColor = prompt("Qual è il tuo cognome?");
 console.log(myColor);
 
 document.getElementById("my-password").innerHTML = firstName + surname + myColor + "21";
