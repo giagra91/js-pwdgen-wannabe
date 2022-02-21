f6869b59af0b32290971157539cbb286f99feb14
giagra91 <giagra91@gmail.com>
2022-02-21 15:08:03 +0100

    modifiche file JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const firstName = prompt("Qual è il tuo nome?")
+console.log(firstName)
+
+const surname = prompt("Qual è il tuo cognome?")
+console.log(surname)
+
+const myColor = prompt("Qual è il tuo cognome?")
+console.log(myColor)
+
+
+
+document.getElementById("my-password").innerHTML = firstName + surname + myColor + "21"
